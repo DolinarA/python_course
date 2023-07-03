93a6de4aba79af216c1ccff086c6a899c9fdb001
Ana <ana.dolinar@nib.si>
2023-07-03 13:14:11 +0200

    data extraction (nanostat)

--- main-EppoWebConverter.py
+++ main-EppoWebConverter.py
@@ -1,11 +1,13 @@
 import os
+import random
 import sys
+import time
 from typing import Union
 
 import pdfkit
 
 BASE_TAXON_URL = "https://gd.eppo.int/taxon/"
-
+BASE_SEARCH_URL = "https://gd.eppo.int/search?k="
 # if system path doesn't work, specify it using WKHTMLTOPDF_EXECUTABLE
 # here and in functions that use convert_webpage_to_pdf function
 # WKHTMLTOPDF_EXECUTABLE = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
@@ -35,6 +37,15 @@ def convert_webpage_to_pdf(
         )
 
 
+def get_taxon_code_from_url(url: str) -> list[str]:
+    pass
+    # search rezultate preprasate v html
+    # request.get(url)
+    # https://realpython.com/beautiful-soup-web-scraper-python/
+    # beautiful soup - html parser
+    # soup = BeautifulSoup(html, "html.parser")
+
+
 def run_code_mode():
     code = input("Enter the code of the taxon: ")
     url = f"{BASE_TAXON_URL}{code.upper()}"
@@ -56,12 +67,23 @@ def run_file_mode():
                 url = f"{BASE_TAXON_URL}{code.upper()}"
                 output_file_path = os.path.join(output_folder_path, f"{code}.pdf")
                 convert_webpage_to_pdf(url, output_file_path)
+                time.sleep(random.randint(2, 7))  # random sleep 2-7 seconds
     else:
         print("Error: input file does not exist.")
 
 
 def run_search_mode():
-    pass
+    input_search_term = input("Enter the search term: ")
+    output_folder_path = input("Enter the path to the output folder: ")
+    validate_folder_path(output_folder_path)
+    url = f"{BASE_SEARCH_URL}{input_search_term}"
+    codes = get_taxon_code_from_url(url)
+    for code in codes:
+        code = code.strip()
+        url = f"{BASE_TAXON_URL}{code.upper()}"
+        output_file_path = os.path.join(output_folder_path, f"{code}.pdf")
+        convert_webpage_to_pdf(url, output_file_path)
+        time.sleep(random.randint(2, 7))
 
 
 def main():
